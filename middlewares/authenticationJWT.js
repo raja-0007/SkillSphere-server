c47145cfcdd083b41a75f3b03e2df30bb3411a0c
raja-0007 <rajadaragani07@gmail.com>
2024-06-15 20:45:11 +0530

    jwt activated
    - jwt added to getcart and removefromcart routes
    - removefromcart route created

--- middlewares/authenticationJWT.js
+++ middlewares/authenticationJWT.js
@@ -1,10 +1,21 @@
+const jwt = require('jsonwebtoken');
+
 const authenticationJWT = (req,res,next)=>{
-    if(req.action == 'login'){
-        console.log('login')
-        next()
-    }
+    const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+    });}
     else{
-        next()
+        res.sendStatus(401);
     }
 }
 module.exports = authenticationJWT
